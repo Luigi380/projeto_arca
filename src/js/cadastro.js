dba959d3b1cdd8cdd9739c8fe458e82d14a84cae
Luigi de Souza <luigi.de.sou@gmail.com>
2026-06-07 23:58:00 -0300

    feat: add cadastro and login pages with associated styles and scripts

---
+++ src/js/cadastro.js
@@ -0,0 +1,24 @@
+var checkTutor = document.getElementById("check-tutor");
+var checkOng = document.getElementById("check-ong");
+var secaoTutor = document.getElementById("secao-tutor");
+var secaoOng = document.getElementById("secao-ong");
+
+checkTutor.addEventListener("change", function () {
+  if (checkTutor.checked) {
+    checkOng.checked = false;
+    secaoTutor.classList.remove("hidden");
+    secaoOng.classList.add("hidden");
+  } else {
+    secaoTutor.classList.add("hidden");
+  }
+});
+
+checkOng.addEventListener("change", function () {
+  if (checkOng.checked) {
+    checkTutor.checked = false;
+    secaoOng.classList.remove("hidden");
+    secaoTutor.classList.add("hidden");
+  } else {
+    secaoOng.classList.add("hidden");
+  }
+});
